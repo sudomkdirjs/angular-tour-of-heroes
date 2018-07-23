0708483797afe9d9fb413aa3ea2ecd2739c286f2
Marees S <mareesvlpt@gmail.com>
2018-07-23 23:15:16 +0530

    MS | done with heroes crud using mock data

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -25,7 +25,6 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    // console.log({id})
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
